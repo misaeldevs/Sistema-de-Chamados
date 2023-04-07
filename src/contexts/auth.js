2c5b157ff476d62b1f16187522aebc6a906d6a59
misaeldevs <misaeldev@outlook.com>
2023-04-07 08:34:37 -0300

    Instalação e configuração do React Toastify para estilização e cadastro do usuário.

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -2,12 +2,15 @@ import { useState, createContext, useEffect } from "react";
 import { auth, db } from "../services/firebaseConnection"
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { doc, getDoc, setDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 export const AuthContext = createContext({})
 
 function AuthProvider({ children }) {
     const [user, setUser] = useState(null)
     const [loadingAuth, setLoadingAuth] = useState(false)
+    const navigate = useNavigate()
 
     function signIn(email, password) {
         console.log(email, password)
@@ -34,13 +37,20 @@ function AuthProvider({ children }) {
                             avatarUrl: null
                         }
                         setUser(data)
+                        storageUser(data)
                         setLoadingAuth(false)
+                        toast.success("Seja bem vindo a nosso sistema!")
+                        navigate("/dashboard")
                     })
                     .catch((error) => {
                         console.log(error)
                         setLoadingAuth(false)
                     })
             })
+    }
+    
+    function storageUser(data){
+        localStorage.setItem("@sistemasChamados", JSON.stringify(data))
     }
 
     return (
