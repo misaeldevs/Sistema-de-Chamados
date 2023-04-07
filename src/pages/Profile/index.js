0eb25cf72d7358fa9b05b767b84acd514ec25561
misaeldevs <misaeldev@outlook.com>
2023-04-07 13:25:32 -0300

    Criação e estilização do Title do Dashboard.

---
+++ src/pages/Profile/index.js
@@ -0,0 +1,19 @@
+import Header from "../../components/Header"
+import Title from "../../components/Title"
+import { FiSettings } from "react-icons/fi"
+
+export default function Profile() {
+    return (
+        <div>
+            <Header />
+
+            <div className="content">
+                <Title name="Meu Perfil">
+                    <FiSettings size={25} />
+                </Title>
+            </div>
+
+            <h1>Página de Perfil</h1>
+        </div>
+    )
+}
