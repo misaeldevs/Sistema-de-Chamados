88cd0b03f03f95fdd1d98aa8f4eaa9d6b9cf2ac2
misaeldevs <misaeldev@outlook.com>
2023-04-08 11:41:25 -0300

    Construção dos elementos da página Dashboard.

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,19 +1,69 @@
 import { useContext } from "react"
 import { AuthContext } from "../../contexts/auth"
+
 import Header from "../../components/Header"
+import Title from "../../components/Title"
+import { FiPlus, FiMessageSquare, FiSearch, FiEdit2 } from "react-icons/fi"
+import { Link } from "react-router-dom"
 
-export default function Dashboard(){
-    const {logout} = useContext(AuthContext)
+import "./dashboard.css"
 
-    async function handleLogOut(){
+export default function Dashboard() {
+    const { logout } = useContext(AuthContext)
+
+    async function handleLogOut() {
         await logout()
     }
-    
-    return(
-        <div>
-            <Header/>
 
-            <h1>Página de Dashboard</h1>
+    return (
+        <div>
+            <Header />
+
+            <div className="content">
+                <Title name="Chamados">
+                    <FiMessageSquare size={25} />
+                </Title>
+
+                <>
+                    <Link to="/new" className="new">
+                        <FiPlus color="#FFF" size={25} />
+                        Abrir Chamado
+                    </Link>
+
+                    <table>
+                        <thead>
+                            <tr>
+                                <th scope="col">Cliente</th>
+                                <th scope="col">Assunto</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Data de Cadastro</th>
+                                <th scope="col">#</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <tr>
+                                <td data-label="Cliente">Colégio Rezende LTDA</td>
+                                <td data-label="Assunto">Suporte</td>
+                                <td data-label="Status">Em progresso</td>
+                                <td data-label="Cadastrado">08/04/2023</td>
+                                <td data-label="#">
+                                    <button className="action" style={{backgroundColor: "#3583f6"}}>
+                                        <FiSearch color="#fff" size={17} />
+                                    </button>
+                                    
+                                    <button className="action" style={{backgroundColor: "#f6a935"}}>
+                                        <FiEdit2 color="#fff" size={17} />
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+
+                    </table>
+                </>
+
+
+            </div>
             <button onClick={handleLogOut}>Sair</button>
         </div>
     )
